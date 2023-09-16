7c9e378cde4236fe44cb14af92616ecf98682ff3
komathi33 <144309872+komathi33@users.noreply.github.com>
2023-09-16 12:53:04 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,22 @@
+class player:
+  def player(self):
+        print("The player is playing cricket")
+
+class Batsman(player):
+  def play(self):
+        print("Batsman is batting.")
+class Bowler(player):
+  def play(self):
+        print("Bowler is bowling. ")
+
+Batsman=Batsman()
+Bowler=Bowler()
+
+Batsman.play()
+Bowler.play()from cmu_graphics import *
+
+# Write your code here
+# Not sure where to start?
+# Check out README.md under "Files"
+
+cmu_graphics.run()
